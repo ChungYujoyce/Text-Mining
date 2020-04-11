69810dbdf2499e004f3fae4066e2db6d65a535e8
graceHunag <joyce.bhps@gmail.com>
2020-04-11 16:11:49 +0800

    finish analyze results.

--- text_your_feeling/textyourfeeling/catalog/views.py
+++ text_your_feeling/textyourfeeling/catalog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
+
 def index(request):
     """View function for home page of site."""
     return render(request, 'index.html')
 
 def gallery(request):
     return render(request, 'pages/gallery.html')
-
