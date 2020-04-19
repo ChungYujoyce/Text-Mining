49a64f1561fa9f315129d1bde495214c61b724dc
graceHunag <joyce.bhps@gmail.com>
2020-04-19 14:52:43 +0800

    remove old web version (Django) , change to Flask

--- text_your_feeling/textyourfeeling/catalog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    """View function for home page of site."""
-    return render(request, 'index.html')
-
-def gallery(request):
-    return render(request, 'pages/gallery.html')
